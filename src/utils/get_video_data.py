c6eda55db72eb392799ad4d99d721be27afedddb
canaleal <alexcanales766@gmail.com>
2022-12-03 18:30:44 -0500

    Removed 2nd get

--- src/utils/get_video_data.py
+++ src/utils/get_video_data.py
@@ -74,38 +74,3 @@ def get_google_video_for_processing(video_link=''):
         logging.error(e)
         return e
 
-
-
-def get_google_video_for_processing(video_link=''):
-    try:
-
-        folder_util.checkIfFolderExistsAndCreateIfNot(data_folder_path)
-        # * Download the file and load the raw data
-        urllib.request.urlretrieve(video_link, video_save_path)
-
-        # * Create the subprocess
-        stride = 1
-        detect_python_path = 'C:/Users/alexc/Documents/MachineLearning/yolov5/detect.py'
-        process = f'python {detect_python_path} --source {video_save_path} --vid-stride {stride} --conf-thres {0.4}'
-        callSubProcess(process)
-
-        # * Get the output
-        path = f'{runs_folder_path}/exp/video.mp4v'
-        
-        newPath = f'{output_folder_path}/video-codec.mp4'
-        newPathCompressed = f'{output_folder_path}/video-codec-comp.mp4'
-               
-        # * Create ffmpeg process
-        callSubProcess(f'ffmpeg -y -i {path} -map 0 -c copy {newPath}')
-        #callSubProcess(f'ffmpeg -y -i {newPath} -vcodec libx265 -crf 28 {newPathCompressed}')
-        #callSubProcess(f'ffmpeg -y -i {newPathCompressed} -map 0 -c copy {newPath}')
-        
-          # * Delete all files (input, output)
-        folder_util.deleteAllFilesInFolder(data_folder_path)
-        folder_util.deleteAllFoldersInFolder(runs_folder_path)
-        
-        return newPath
-      
-    except Exception as e:
-        logging.error(e)
-        return e
